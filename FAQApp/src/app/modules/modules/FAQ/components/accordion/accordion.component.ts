c68cc327c47895a34fbecf8c6a4a94363a887d90
Enoch Offiah <enoch.offiah@humley.com>
2020-09-14 09:29:29 +0100

    component stylings, svg icon added, work on animating the icon

--- FAQApp/src/app/modules/modules/FAQ/components/accordion/accordion.component.ts
+++ FAQApp/src/app/modules/modules/FAQ/components/accordion/accordion.component.ts
@@ -6,30 +6,15 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./accordion.component.sass'],
 })
 export class AccordionComponent implements OnInit {
-  // @Input() opened = false;
-  // @Input() title: string;
-  // @Output() toggle: EventEmitter<any> = new EventEmitter<any>();
+
+  activeAccordion = 'One'
 
   constructor() {}
 
   ngOnInit() {
   }
 
-  // should use the index to toggle between accordion
-  getAccordionToggle() {
-    var acc = document.getElementsByClassName('accordion');
-    var i;
+  isActive = (name: string) => this.activeAccordion === name
 
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener('click', function () {
-        this.classList.toggle('active');
-        var panel = this.nextElementSibling;
-        if (panel.style.display === 'block') {
-          panel.style.display = 'none';
-        } else {
-          panel.style.display = 'block';
-        }
-      });
-    }
-  }
+  setActive = (name: string) => this.activeAccordion = name
 }
